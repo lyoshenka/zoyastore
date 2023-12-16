c355aa2437b3dcd3bd196c3889d7407ed485c1f4
Alex Grintsvayg <git@grin.io>
2023-12-16 10:33:11 -0500

    Deploy

--- _next/static/chunks/app/layout-f651d355e6b81097.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{259:function(n,a,e){Promise.resolve().then(e.t.bind(e,6087,23))},6087:function(n){n.exports={style:{fontFamily:"'__Inter_71afa5', '__Inter_Fallback_71afa5'",fontStyle:"normal"},className:"__className_71afa5"}}},function(n){n.O(0,[971,938,744],function(){return n(n.s=259)}),_N_E=n.O()}]);
